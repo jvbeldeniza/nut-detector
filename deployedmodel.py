5b4b558eb252f1df9e465231f93430ef67f6ac65
jvbeldeniza <qjvbbeldeniza@tip.edu.ph>
2023-12-16 18:01:33 +0800

    Update deployedmodel.py

--- deployedmodel.py
+++ deployedmodel.py
@@ -15,8 +15,6 @@ st.divider()
 st.write('To avoid error, please ensure that the color depth is set to 24 bit & to upload a square image e.g. 100x100, 60x60.')
 st.divider()
 
-
-
 if 'checkbox_1_selected' not in st.session_state:
     st.session_state.checkbox_1_selected = False
 
